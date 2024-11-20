eaffc62e51ece64c8febbfdb25f75ca10ce53ca4
mareez01 <siva22012005@gmail.com>
2024-11-20 10:08:50 +0530

    Updated

--- userinteraction.js
+++ userinteraction.js
@@ -360,10 +360,9 @@ const submitDetails = async (chatId, messageId) => {
     await Promise.all([
         //botStatus(chatId, 'typing'),
         sendDataToSheet(userInputData),
-        editMessage(chatId, sucMsg, init.message_id, reply_markup, 'MarkdownV2'),
  // Send user info to the server;
     ]);
-      
+    await editMessage(chatId, sucMsg, init.message_id, reply_markup, 'MarkdownV2')
       
 
 }
